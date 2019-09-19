e4887e11e36a122345ac6baa054f78ad2c741122
Darren-Steve <50610753+darren2025@users.noreply.github.com>
2019-09-19 15:08:49 +0800

    测试没有问题
    测试没有问题

--- LNMP.sh
+++ LNMP.sh
@@ -6,12 +6,18 @@ wget https://raw.githubusercontent.com/darren2025/LNMP-/master/nginx
 wget https://raw.githubusercontent.com/darren2025/LNMP-/master/nginx.service
 wget https://raw.githubusercontent.com/darren2025/LNMP-/master/nginx-1.16.1.tar.gz
 wget https://raw.githubusercontent.com/darren2025/LNMP-/master/php-7.3.2.tar.gz
-wget https://raw.githubusercontent.com/darren2025/LNMP-/master/php.sh
+#安装mysql
+#下载
+yum install -y mariadb-server
+#启动
+systemctl start mariadb.service
+#添加开机启动
+systemctl enable mariadb.service
+
 cp nginx /etc/init.d
 cp nginx nginx.service /usr/lib/systemd/system
 chmod +x nginx /etc/init.d/nginx
-chmod +x php.sh 
-sh php.sh
+
 #安装nginx
 #添加用户和用户组 设置程序启动的用户
 groupadd www
@@ -26,6 +32,50 @@ cd nginx-1.16.1
 --with-http_ssl_module \
 #编译 && 安装
 make && make install
+
+
+
+#安装PHP
+#解压进入解压目录
+cd /root
+tar -zxvf php-7.3.2.tar.gz
+cd php-7.3.2
+#配置
+./configure --prefix=/usr/local/php_nginx \
+--with-config-file-path=/usr/local/php/etc \
+--with-fpm-user=www \
+--with-fpm-group=www \
+--with-pdo-mysql=mysqlnd \
+--with-mysqli=mysqlnd \
+--with-freetype-dir \
+--with-gd \
+--with-zlib \
+--with-libxml-dir \
+--with-jpeg-dir \
+--with-png-dir \
+--enable-mbstring=all \
+--enable-mbregex \
+--enable-shared \
+--enable-fpm \
+--without-pear
+make && make install
+cp /usr/local/php_nginx/etc/php-fpm.conf.default /usr/local/php_nginx/etc/php-fpm.conf
+cp /usr/local/php_nginx/etc/php-fpm.d/www.conf.default /usr/local/php_nginx/etc/php-fpm.d/www.conf
+cp php.ini-development /usr/local/php_nginx/etc/php.ini
+cp sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
+chmod +x /etc/init.d/php-fpm
+chkconfig --add php-fpm
+chkconfig php-fpm on
+echo 'PATH=/usr/local/php/bin:$PATH' >> /etc/profile
+source /etc/profile
+
+
+
+
+
+service php-fpm start
+#添加开机自启
+systemctl enable php-fpm.service
 systemctl daemon-reload
 service nginx start
 #添加开机自启
@@ -35,39 +85,8 @@ iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 service iptables save 
 service iptables restart 
 
-
-
-v=`cat /etc/redhat-release|sed -r 's/.* ([0-9]+)\..*/\1/'`
- if [ $v -eq 6 ]; then
-#centos6
-#下载文件
-wget https://raw.githubusercontent.com/darren2025/mysql5.1-5.6/master/install_mysql.sh
-#给予权限
-chmod +x install_mysql.sh
-#执行:
-sh install_mysql.sh
-#开启自启
-chkconfig --add mysqld
-chkconfig --level 35 mysqld on
 #初始化数据库
 mysql_secure_installation
-fi
- 
-#  centos-7:
- 
-if [ $v -eq 7 ]; then
- 
-#安装mysql
-#下载
-yum install -y mariadb-server
-#启动
-systemctl start mariadb.service
-#添加开机启动
-systemctl enable mariadb.service
-#初始化数据库
-mysql_secure_installation
-fi
-
 
 
 
